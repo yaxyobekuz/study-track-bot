083ffb4cea09c1c70c99ceffa437c195d2b6b7ce
yaxyobekuz <pubgn9642@gmail.com>
2026-03-18 13:54:42 +0500

    feat: remove enum constraint from role field in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,7 +43,6 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["owner", "teacher", "student"],
       required: [true, "Rol majburiy"],
     },
     classes: [
